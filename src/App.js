743042534f1aa946ea8e4dd8a64a5c98fb2d0a41
Vittoz <vittoz@linux.home>
2020-12-03 19:41:34 -0500

    debut de la page d'accueil

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './Logo.png';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 
 function App() {
   return (
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          bonjour...?
+        </p>
+        <a>
+         un bouton
+        </a>
+      </header>
+    </div>
   );
 }
 
